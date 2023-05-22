0f7b927838811c1ea4e48f58993dd392d8da44d7
Jackson Ramos <jackson.ramoos@outlook.com>
2023-05-22 12:11:39 -0300

    first commit - frontend

---
+++ web/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <h1>
+      Hello World!
+      <Button title="Jackson"/>
+      <Button title="Yusuke"/>
+      <Button title="Gon"/>
+    </h1>
+  )
+}
